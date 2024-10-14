21e2e961659b2af80a27172c356a406c0211a374
Rodrigo Alvarez <roallamos@gmail.com>
2024-10-14 12:16:37 +0200

    ya replaza todo falta separar en funciones

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,10 +6,14 @@ int main(int argc, char *argv[])
     std::ifstream       infile;
     std::ofstream       outfile;
     std::string         str;
+    std::string         str_replace;
+    std::string         s1 = argv[2];
+    std::string         s2 = argv[3];
     std::string         new_str;
     std::string         name;
-    int                 start;
+    unsigned long int   start;
     long unsigned int   i = 0;
+    long unsigned int   j = 0;
 
     if (argc != 4)
         return (0);
@@ -17,26 +21,33 @@ int main(int argc, char *argv[])
     name.append(argv[1]);
     name.append(".replace");
     outfile.open(name.c_str());
-    if (!infile)
-		std::cout << "File not created!";
-	else
+	std::cout << "File created successfully!\n";
+    while (std::getline(infile,new_str))
+   {
+        str += new_str;
+        if (!infile.eof())
+            str += '\n';
+    }
+    while (str.find(s1) != (long unsigned int)-1)
     {
-		std::cout << "File created successfully!\n";
-        while (std::getline(infile,new_str))
-        {
-            str += new_str;
-            if (!infile.eof())
-                str += '\n';
-        }
+        i = 0;
         while (i != str.length())
         {
-            outfile << str.at(i);
-            start = str.find(argv[2]);
-            i++;
+            j = 0;
+            start = str.find(s1);
+            std::cout << start << std::endl;
+            while (i != str.length() && i != start)
+                str_replace += str.at(i++);
+            while (i == start && j != s2.length())
+                str_replace += s2.at(j++);
+            if (j != 0)
+                i += s1.length();
         }
-        std::cout << start << std::endl;
-        std::cout << str << std::endl;
-		infile.close(); 
-	}
+        str = str_replace;
+        str_replace.clear();
+    }
+    outfile << str;
+	infile.close();
+    outfile.close();
     return (0);
 }

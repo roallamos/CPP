7a97618733c7a4cd566815cc40e71c3a2df0e37f
Rodrigo Alvarez <roallamos@gmail.com>
2024-12-16 19:34:01 +0100

    virtual añadido a las clases hijas y correcta implementacion de los constructores de copia

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rodralva <rodralva@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:47:16 by rodralva          #+#    #+#             */
-/*   Updated: 2024/12/16 17:10:45 by rodralva         ###   ########.fr       */
+/*   Updated: 2024/12/16 19:32:18 by rodralva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ const Animal* meta = new Animal();
 const Animal* j = new Dog();
 const Animal* i = new Cat();
 Dog perro;
-Dog copy;
 
 perro.setIdeas("⚽");
+Dog copy(perro);
 copy = perro;
 std::cout << "--------ideas perro--------\n";
 perro.printIdeas();
